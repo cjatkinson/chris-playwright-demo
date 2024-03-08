ee88903028fd85c20b9a851bf5f6bb9173e034ba
Christopher Atkinson <chris.atkinson@bit.ly>
2024-03-08 09:18:35 -0500

    fix comment

--- pages/bitly/app.home.page.ts
+++ pages/bitly/app.home.page.ts
@@ -3,7 +3,7 @@ import type { Locator, Page } from '@playwright/test';
 export class AppHome {
   constructor(private page: Page) {}
 
-  // selectors //
+  // locators //
 
   public header: Locator = this.page.getByRole('heading', { name: 'Your Connections Platform' });
 
